0cf0ba34079b492b437ede8c64f3bd70460f742c
Greg Piccolo <greg.piccolo@gmail.com>
2016-04-09 19:49:01 -0400

    turn strict_slashes off on routes

--- api/controllers/hashes.py
+++ api/controllers/hashes.py
@@ -6,14 +6,14 @@ from api.models.hash import Hash
 hashes = Blueprint("hash", __name__)
 
 
-@hashes.route("/", methods=["POST"])
+@hashes.route("/", methods=["POST"], strict_slashes=False)
 @request_is_json
 def post_hash():
     hash_output = current_app.db.save(request.get_json())
     return make_response(jsonify({"hash": hash_output}), 201)
 
 
-@hashes.route("/<hashstring>", methods=["GET"])
+@hashes.route("/<hashstring>", methods=["GET"], strict_slashes=False)
 def get_hash(hashstring):
     json_data = current_app.db.get(hashstring)
     if(json_data is None):
